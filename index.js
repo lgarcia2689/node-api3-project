6beb942a46006fd6726e026b6462168bbc92b687
unknown <you@example.com>
2021-05-06 18:36:49 -0500

    changed made

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const server = require('./api/server');
 
 require('dotenv').config()
 
-const PORT = process.env.PORT 
+const port = process.env.PORT || 4000
 
-server.listen(PORT, () => {
-  console.log(`\n* Server Running on http://localhost:${PORT} *\n`);
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
